80928a8f19d05178e6b5375fb8c0a72c5b93594e
unknown <matheus.aacandido1@CAS419596W0705.senacsp.edu.br>
2016-02-16 21:27:37 -0200

    Add animation for run, idle and jump Add the class for controlling the player

---
+++ Assets/Script/Megaman/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public Rigidbody2D _body = null;
+	public Animator _animator = null;
+	public float _velocity = 10.0f;
+	public float _velocityJump = 10.0f;
+
+	void Start () 
+	{
+		this._body = this.GetComponent<Rigidbody2D> ();
+		this._animator = this.GetComponent<Animator> ();
+	}
+
+	void Update ()
+	{
+		float horizontal = Input.GetAxis ("Horizontal");
+		if (horizontal != 0) 
+		{
+			if (this.transform.localScale.x < 0 && horizontal > 0 || this.transform.localScale.x > 0 && horizontal < 0) 
+			{
+				this.transform.localScale = new Vector3(this.transform.localScale.x * -1, this.transform.localScale.y, this.transform.localScale.z);
+			}
+		}
+
+		this._animator.SetFloat ("velocity", Mathf.Abs(this._body.velocity.x));
+
+		float velocity = ((this._velocity * horizontal)- this._body.velocity.x) * 100;
+		this._body.AddForce(Vector2.right * velocity * Time.deltaTime, ForceMode2D.Force);
+
+		if (Input.GetButton ("Jump")) {
+			if (this._body.velocity.y == 0) {
+				this._animator.SetBool ("jumping", true);
+				this._body.AddForce (Vector2.up * (this._velocityJump * Time.deltaTime), ForceMode2D.Impulse);
+			}
+		}
+		if (this._body.velocity.y == 0) {
+			this._animator.SetBool ("jumping", false);
+		}
+	}
+}
