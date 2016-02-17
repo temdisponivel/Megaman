fd985a1aeca2289230c4cf01f4d011ad5a1a5d27
unknown <matheus.aacandido1@CAS419596W0705.senacsp.edu.br>
2016-02-16 22:08:09 -0200

    Add some friction to foot of megaman

--- Assets/Script/Megaman/PlayerController.cs
+++ Assets/Script/Megaman/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 	public SpriteRenderer _spriteRenderer = null;
 	public float _velocity = 10.0f;
 	public float _velocityJump = 10.0f;
+	private float _forceFrame = 0;
+	private bool _jump = false;
 
 	void Start () 
 	{
@@ -27,19 +29,29 @@ public class PlayerController : MonoBehaviour
 			}
 		}
 
-		this._animator.SetFloat ("velocity", Mathf.Abs(horizontal * 1.5f));
+		this._animator.SetFloat ("velocity", Mathf.Abs(horizontal));
 
-		float velocity = ((this._velocity * horizontal)- this._body.velocity.x) * 100;
-		this._body.AddForce(Vector2.right * velocity * Time.deltaTime, ForceMode2D.Force);
+		this._forceFrame = ((this._velocity * horizontal)- this._body.velocity.x);
+
 
 		if (Input.GetButton ("Jump")) {
 			if (this._body.velocity.y == 0) {
-				this._animator.SetBool ("jumping", true);
-				this._body.AddForce (Vector2.up * (this._velocityJump * Time.deltaTime), ForceMode2D.Impulse);
+				this._jump = true;
 			}
 		}
 		if (this._body.velocity.y == 0) {
 			this._animator.SetBool ("jumping", false);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		this._body.AddForce(Vector2.right * this._forceFrame, ForceMode2D.Force);
+		if (this._jump) 
+		{
+			this._body.AddForce (Vector2.up * (this._velocityJump * Time.deltaTime), ForceMode2D.Force);
+			this._animator.SetBool ("jumping", true);
+			this._jump = false;
 		}
 	}
 }
