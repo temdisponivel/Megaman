9c57d4a71dc2fa6cce9b07fdaa7a7969a45620a1
Matheus <temdisponivel@gmail.com>
2016-02-24 06:05:01 -0300

    Make a follow camera controller; Adjust some properties of mega man;

---
+++ Assets/Script/Camera/FollowPlayer2D.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer2D : MonoBehaviour
+{
+    public enum UpdateType
+    {
+        Update,
+        FixedUpdate,
+        LateUpdate,
+    }
+
+    public GameObject _target = null;
+    public float _velocity = 1.0f;
+    [Tooltip("The distance that this camera will mantain to the tager. Zero to center.")]
+    public Vector2 _ditance = new Vector2();
+    public bool _targetPlayer = false;
+    public UpdateType _updateType = UpdateType.Update;
+
+    void Start()
+    {
+        if (_targetPlayer && _target == null)
+        {
+            this._target = GameObject.FindWithTag("Player");
+        }
+    }
+
+    void Update()
+    {
+        if (this._updateType != UpdateType.Update)
+        {
+            return;
+        }
+        this.transform.position = this.GetNextPosition(Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+        if (this._updateType != UpdateType.LateUpdate)
+        {
+            return;
+        }
+        this.transform.position = this.GetNextPosition(Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        if (this._updateType != UpdateType.FixedUpdate)
+        {
+            return;
+        }
+        this.transform.position = this.GetNextPosition(Time.fixedDeltaTime);
+    }
+
+    protected Vector3 GetNextPosition(float deltaTime)
+    {
+        this.GetTarget();
+        if (this._target == null)
+        {
+            return this.transform.position;
+        }
+        Vector2 difference = this.transform.position - this.transform.InverseTransformDirection(this._target.transform.position);
+        Vector2 newPosition = Vector2.MoveTowards(this.transform.position, (Vector2)this._target.transform.position + this._ditance, (difference - this._ditance).magnitude * this._velocity * deltaTime);
+        return new Vector3(newPosition.x, newPosition.y, this.transform.position.z);
+    }
+
+    protected void GetTarget()
+    {
+        if (this._targetPlayer)
+        {
+            this._target = GameObject.FindWithTag("Player");
+        }
+    }
+}
--- Assets/Script/Megaman/PlayerController.cs
+++ Assets/Script/Megaman/PlayerController.cs
@@ -3,12 +3,11 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour 
 {
-	public Rigidbody2D _body = null;
-	public Animator _animator = null;
-	public SpriteRenderer _spriteRenderer = null;
+	protected Rigidbody2D _body = null;
+    protected Animator _animator = null;
+    protected SpriteRenderer _spriteRenderer = null;
 	public float _velocity = 10.0f;
 	public float _velocityJump = 10.0f;
-	private float _forceFrame = 0;
 	private bool _jump = false;
 
 	void Start () 
@@ -31,9 +30,6 @@ public class PlayerController : MonoBehaviour
 
 		this._animator.SetFloat ("velocity", Mathf.Abs(horizontal));
 
-		this._forceFrame = ((this._velocity * horizontal)- this._body.velocity.x);
-
-
 		if (Input.GetButton ("Jump")) {
 			if (this._body.velocity.y == 0) {
 				this._jump = true;
@@ -46,10 +42,11 @@ public class PlayerController : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		this._body.AddForce(Vector2.right * this._forceFrame, ForceMode2D.Force);
+        Vector2 inpulseDirection = Vector2.right * ((this._velocity * Input.GetAxis("Horizontal")) - this._body.velocity.x);
+        this._body.AddForce(inpulseDirection, ForceMode2D.Impulse);
 		if (this._jump) 
 		{
-			this._body.AddForce (Vector2.up * (this._velocityJump * Time.deltaTime), ForceMode2D.Force);
+			this._body.AddForce (Vector2.up * (this._velocityJump), ForceMode2D.Impulse);
 			this._animator.SetBool ("jumping", true);
 			this._jump = false;
 		}
