23abf608856f51a86879957c9bbe21269b8e7e2e
Matheus <temdisponivel@gmail.com>
2016-02-27 00:41:28 -0300

    Add functionality of freeze the camera in x or y (or both) axes;

--- Assets/Script/Camera/FollowPlayer2D.cs
+++ Assets/Script/Camera/FollowPlayer2D.cs
@@ -13,9 +13,12 @@ public class FollowPlayer2D : MonoBehaviour
     public GameObject _target = null;
     public float _velocity = 1.0f;
     [Tooltip("The distance that this camera will mantain to the tager. Zero to center.")]
-    public Vector2 _ditance = new Vector2();
+    public Vector2 _distance = new Vector2();
     public bool _targetPlayer = false;
     public UpdateType _updateType = UpdateType.Update;
+    [Header("Freeze Axis")]
+    public bool _freezeX = false;
+    public bool _freezeY = false;
 
     void Start()
     {
@@ -60,7 +63,10 @@ public class FollowPlayer2D : MonoBehaviour
             return this.transform.position;
         }
         Vector2 difference = this.transform.position - this.transform.InverseTransformDirection(this._target.transform.position);
-        Vector2 newPosition = Vector2.MoveTowards(this.transform.position, (Vector2)this._target.transform.position + this._ditance, (difference - this._ditance).magnitude * this._velocity * deltaTime);
+        Vector2 targetPosition = this._target.transform.position;
+        if (this._freezeX) { targetPosition.x = this.transform.position.x - this._distance.x; }
+        if (this._freezeY) { targetPosition.y = this.transform.position.y - this._distance.y; }
+        Vector2 newPosition = Vector2.MoveTowards(this.transform.position, targetPosition + this._distance, (difference - this._distance).magnitude * this._velocity * deltaTime);
         return new Vector3(newPosition.x, newPosition.y, this.transform.position.z);
     }
 
